dbe274f5316eaaadea12994c5477d0918f57a051
GuilhermeProença <127969945+GuilhermeProencaCorrea@users.noreply.github.com>
2023-04-02 18:20:44 -0300

    :white_check_mark: Concluido
    Desafio-1 (Primeiro Game UNIFAA)

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const personagem = document.querySelector('.personagem');
 const cacto = document.querySelector('.cacto');
+const score = document.querySelector(".score");
+
+function myt(){
+const audio = document.querySelector('audio')
+audio.play()
+}
+let conte = 0; 
 
 const pulo = () => {
     personagem.classList.add('pulo');
@@ -15,7 +22,8 @@ const loop = setInterval(() => {
 
     const cactoPosição = cacto.offsetLeft;
     const personagemPosição = +window.getComputedStyle(personagem).bottom.replace('px','');
-
+    
+ 
     if(cactoPosição <= 180 && cactoPosição > 0 && personagemPosição < 100){
 
         cacto.style.animation = 'none';
@@ -23,21 +31,34 @@ const loop = setInterval(() => {
 
         personagem.style.animation = 'none';
         personagem.style.bottom = `${personagemPosição}px`;
+
+        
+        
         
         personagem.src = './img/morte.gif';
         personagem.style.width = '350px';
         personagem.style.marginleft = '22px auto' 
         personagem.style.marginheith = '22px'
-        /*personagem.style.bottom = '-2px'*/
-       
-
-
+        
+        
 
         clearInterval(loop);
-
     }
+    conte++;
+    score.innerHTML = `score: ${conte}`;
+    
 
 
 },10);
 
+
 document.addEventListener('keydown', pulo)
+
+//Restart da página
+
+const bRestart = document.querySelector("#restart")
+bRestart.addEventListener('click',() => {
+    location.reload()
+});
+
+
