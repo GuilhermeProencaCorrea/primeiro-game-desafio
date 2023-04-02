53be745dec07185bb5e2c13036e08df29e91ab56
GuilhermeProença <127969945+GuilhermeProencaCorrea@users.noreply.github.com>
2023-04-02 16:36:34 -0300

    :building_construction: Plano C
    Construindo o plano C

--- index.js
+++ index.js
@@ -1,117 +1,43 @@
-var myGamePiece;
-var myObstacles = [];
-var myScore;
+const personagem = document.querySelector('.personagem');
+const cacto = document.querySelector('.cacto');
 
-function startGame() {
-    myGamePiece = new component(30, 30, "red", 10, 120);
-    myGamePiece.gravity = 1.05;
-    myScore = new component("30px", "Sans-serif", "white", 200, 200, "text");
-    myGameArea.start();
+const pulo = () => {
+    personagem.classList.add('pulo');
+
+    setTimeout(() => {
+
+        personagem.classList.remove('pulo');
+        
+    }, 500);
 }
 
-var myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
-        this.canvas.width = 680;
-        this.canvas.height = 470;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.frameNo = 0;
-        this.interval = setInterval(updateGameArea, 20);
-        },
-    clear : function() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
-}
+const loop = setInterval(() => {
 
-function component(width, height, color, x, y, type) {
-    this.type = type;
-    this.score = 0;
-    this.width = width;
-    this.height = height;
-    this.speedX = 0;
-    this.speedY = 0;    
-    this.x = x;
-    this.y = y;
-    this.gravity = 0;
-    this.gravitySpeed = 0;
-    this.update = function() {
-        ctx = myGameArea.context;
-        if (this.type == "text") {
-            ctx.font = this.width + " " + this.height;
-            ctx.fillStyle = color;
-            ctx.fillText(this.text, this.x, this.y);
-        } else {
-            ctx.fillStyle = color;
-            ctx.fillRect(this.x, this.y, this.width, this.height);
-        }
-    }
-    this.newPos = function() {
-        this.gravitySpeed += this.gravity;
-        this.x += this.speedX;
-        this.y += this.speedY + this.gravitySpeed;
-        this.hitBottom();
-    }
-    this.hitBottom = function() {
-        var rockbottom = myGameArea.canvas.height - this.height;
-        if (this.y > rockbottom) {
-            this.y = rockbottom;
-            this.gravitySpeed = 0;
-        }
-    }
-    this.crashWith = function(otherobj) {
-        var myleft = this.x;
-        var myright = this.x + (this.width);
-        var mytop = this.y;
-        var mybottom = this.y + (this.height);
-        var otherleft = otherobj.x;
-        var otherright = otherobj.x + (otherobj.width);
-        var othertop = otherobj.y;
-        var otherbottom = otherobj.y + (otherobj.height);
-        var crash = true;
-        if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
-            crash = false;
-        }
-        return crash;
-    }
-}
+    const cactoPosição = cacto.offsetLeft;
+    const personagemPosição = +window.getComputedStyle(personagem).bottom.replace('px','');
 
-function updateGameArea() {
-    var x, height, gap, minHeight, maxHeight, minGap, maxGap;
-    for (i = 0; i < myObstacles.length; i += 1) {
-        if (myGamePiece.crashWith(myObstacles[i])) {
-            return;
-        } 
-    }
-    myGameArea.clear();
-    myGameArea.frameNo += 1;
-    if (myGameArea.frameNo == 1 || everyinterval(150)) {
-        x = myGameArea.canvas.width;
-        minHeight = 20;
-        maxHeight = 200;
-        height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
-        minGap = 50;
-        maxGap = 200;
-        gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
-        //Obstaculo do jogo
-        myObstacles.push(new component(10, height, "green", x, 0));
-        myObstacles.push(new component(10, x - height - gap, "green", x, height + gap));
-    }
-    for (i = 0; i < myObstacles.length; i += 1) {
-        myObstacles[i].x += -1;
-        myObstacles[i].update();
-    }
-    myScore.text="SCORE: " + myGameArea.frameNo;
-    myScore.update();
-    myGamePiece.newPos();
-    myGamePiece.update();
-}
+    if(cactoPosição <= 180 && cactoPosição > 0 && personagemPosição < 100){
 
-function everyinterval(n) {
-    if ((myGameArea.frameNo / n) % 1 == 0) {return true;}
-    return false;
-}
+        cacto.style.animation = 'none';
+        cacto.style.left = `${cactoPosição}px`;
 
-function accelerate(n) {
-    myGamePiece.gravity = n;
-}
+        personagem.style.animation = 'none';
+        personagem.style.bottom = `${personagemPosição}px`;
+        
+        personagem.src = './img/morte.gif';
+        personagem.style.width = '350px';
+        personagem.style.marginleft = '22px auto' 
+        personagem.style.marginheith = '22px'
+        /*personagem.style.bottom = '-2px'*/
+       
+
+
+
+        clearInterval(loop);
+
+    }
+
+
+},10);
+
+document.addEventListener('keydown', pulo)
