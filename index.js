b6f00720be808dc8d88f5d9a180a041fc3e4b983
GuilhermeProença <127969945+GuilhermeProencaCorrea@users.noreply.github.com>
2023-05-24 20:59:53 -0300

    .

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-{const personagem = document.querySelector('.personagem');
+const personagem = document.querySelector('.personagem');
 const cacto = document.querySelector('.cacto');
 const score = document.querySelector(".score");
 let conte = 0; 
@@ -18,7 +18,7 @@ const loop = setInterval(() => {
     const cactoPosição = cacto.offsetLeft;
     const personagemPosição = +window.getComputedStyle(personagem).bottom.replace('px','');
     
- 
+    //verifica se o personagem pulo ou não o obstaculo.
     if(cactoPosição <= 180 && cactoPosição > 0 && personagemPosição < 100){
 
         cacto.style.animation = 'none';
@@ -27,18 +27,14 @@ const loop = setInterval(() => {
         personagem.style.animation = 'none';
         personagem.style.bottom = `${personagemPosição}px`;
 
-        
-        
-        
         personagem.src = './img/morte.gif';
         personagem.style.width = '350px';
         personagem.style.marginleft = '22px auto' 
         personagem.style.marginheith = '22px'
         
-        
-
         clearInterval(loop);
     }
+    //pontuação
     conte++;
     score.innerHTML = `score: ${conte}`;
     
@@ -46,14 +42,13 @@ const loop = setInterval(() => {
 
 },10);
 
-
+//evento de pulo, ao pressionar qualquer tecla para baixo o personagem executa um pulo.
 document.addEventListener('keydown', pulo)
 
-//Restart da página
-
+//restart da página
 const bRestart = document.querySelector("#restart")
 bRestart.addEventListener('click',() => {
     location.reload()
-});}
+});
 
 
